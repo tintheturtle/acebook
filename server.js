6cadc9f096076a3ce0a1c7883ebe0b589d769235
tintheturtle <tinhoang@gmail.com>
2020-03-15 21:06:35 -0400

    added user schema

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') 
 const mongoose = require('mongoose') 
 const bodyParser = require('body-parser')
 
@@ -14,13 +14,13 @@ app.use(
   // DB Config
  const uri = "mongodb+srv://Tin-user-44132:klC9hzPccAK17Zn2@acebook-ogevm.mongodb.net/test?retryWrites=true&w=majority";
 
-  // Connect to MongoDB
-  mongoose
-    .connect(
-      uri,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
-  const port = process.env.PORT || 5000; 
-  app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+// Connect to MongoDB
+mongoose
+   .connect(
+    uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+   .then(() => console.log("MongoDB successfully connected"))
+   .catch(err => console.log(err));
+const port = process.env.PORT || 5000; 
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
