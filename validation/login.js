d270d26d149b02c1715be8c93416c6cf1f973411
tintheturtle <tinhoang@gmail.com>
2020-03-15 22:36:25 -0400

    added login validation

---
+++ validation/login.js
@@ -0,0 +1,23 @@
+import Validator from 'validator'
+import isEmpty from 'isEmpty'
+
+const validateLoginInput = (data) => {
+    let errors = {}
+
+    // Email checks
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required";
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
+    // Password checks
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required";
+    }
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+export default validateLoginInput
