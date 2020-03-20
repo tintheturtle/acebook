11c5b2a1c148dca94cece71f9495dbe3fc3ae475
tintheturtle <tinhoang@bu.edu>
2020-03-20 11:23:12 -0400

    added redux-persist to store

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import jwt_decode from 'jwt-decode'
 import setAuthToken from "./utils/setAuthToken"
 import { setCurrentUser, logoutUser } from "./actions/authActions"
 
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
-import store from './store'
+// import store from './store'
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -13,20 +14,17 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import PrivateRoute from "./components/private-route/PrivateRoute"
 import Dashboard from "./components/dashboard/Dashboard"
+
+import { store, persistor } from './store'
+
+
+
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
   // Set auth token header auth
   const token = localStorage.jwtToken
   setAuthToken(token)
-  // Decode token and get user info and exp
   const decoded = jwt_decode(token)
-
-  // Set user and isAuthenticated
-
-  console.log('this is auth')
-  console.log(store.getState().auth.user)
-
-  store.dispatch(setCurrentUser(decoded))
   
   // Check for expired token
   const currentTime = Date.now() / 1000 // to get in milliseconds
@@ -41,18 +39,20 @@ if (localStorage.jwtToken) {
 function App() {
     return (
       <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing}/>
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
-            {/* <Route exact path="/ACE" component={ACE}/> */}
-            <Switch>
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </div>
-        </Router> 
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <div className="App">
+              <Navbar />
+              <Route exact path="/" component={Landing}/>
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+              {/* <Route exact path="/ACE" component={ACE}/> */}
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+            </div>
+          </Router> 
+        </PersistGate>
       </Provider>
       
     )
--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -10,7 +10,7 @@ const initialState = {
     loading: false,
 }
 
-export default function(state = initialState, action) {
+const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_CURRENT_USER:    
             return {
@@ -27,3 +27,5 @@ export default function(state = initialState, action) {
             return state
     }
 }
+
+export default authReducer
