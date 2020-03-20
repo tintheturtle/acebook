f4cfb27db726418d193251105e67236157d990bd
tintheturtle <tinhoang@bu.edu>
2020-03-20 16:15:30 -0400

    added card to dashboard

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,17 @@
 import React  from 'react'
+import '../styles/Card.css'
+import ProfileImage from '../images/profile.png'
 
 const Card = ({data}) => {
     return (
-        <div>
-            <p> {data.name} </p>
+        <div className="card-div">
+            <div className="card-info">
+                <img src={ProfileImage} className="match-image" alt="profile" />
+                <p className="match-name match-info"> {data.name} </p>
+                <p className="match-email match-info"> {data.email} </p> 
+                <p className="match-percentage match-info"> {(data.percentage.toFixed(4) * 100).toString()}% match</p>
+            </div>
+            
         </div>
     )
 }
