2891cbc912cab8972328159a31d379c3a6ed678b
tintheturtle <tinhoang@bu.edu>
2020-03-28 15:31:47 -0400

    added basic display of family matching

--- client/src/components/family/Family.js
+++ client/src/components/family/Family.js
@@ -26,41 +26,74 @@ class Family extends Component {
         
         const { family } = this.props.family
         const { memberObjects } = family
-        console.log(memberObjects)
 
         return(
             <div style={{ height: "100vh" }} className="container">
-                <div id="message-header" className="message-header-row row">
-                    <div className="col s12 center-align">
-                        <h4>
-                            <b>Welcome to the Family Dashboard </b>  
-                            <p className="flow-text grey-text text-darken-1">
-                            </p>
-                        </h4>
-                    </div>
-                </div>
-                <div className="row">
-                    {
-                        !family ? (
-                            <div>
-                                You have not been matched with a family yet
+                {
+                            !family ? 
+                    (
+                        <div id="message-header" className="message-header-row row">
+                            <div className="col s12 center-align">
+                                <h4>
+                                    <b> Hello, </b>  {this.props.auth.user.name}
+                                </h4>
+                                <p className="flow-text grey-text text-darken-1">
+                                    You have not yet been matched, please check back again later.
+                                </p>
                             </div>
-                        ) : 
-                        <div>
-                            { memberObjects.map((data, indx) => {
-                                if (data.email !== this.props.auth.user.email) {
-                                    return (
-                                        <h5 key={indx}> {data.name} </h5> 
-                                    )
-                                }
-                                return ''
-                           
-                            })}
                         </div>
-                    }
-                </div>
+                     ) : 
+                (<>
+                    <div id="message-header" className="message-header-row row">
+                        <div className="col s12 center-align">
+                            <h4>
+                                <b>Welcome to your Family Dashboard </b>  
+                                <p className="flow-text grey-text text-darken-1">
+                                </p>
+                            </h4>
+                        </div>
+                    </div>
+                    <div className="row">
+                        
+                        <div>
+                            <div className="col m6" style={{ background: '#ffb0cd', height: "50vh"}}>
+                                <div className="col s12 center-align">                                
+                                    <div>
+                                        { memberObjects.map((data, indx) => {
+                                            if (data.email !== this.props.auth.user.email) {
+                                                return (
+                                                    <h5 key={indx}> {data.name} </h5> 
+                                                )
+                                            }
+                                            return ''
+                                    
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="col m6" style={{ background: '#ffb0cd', height: "50vh"}}>
+                                <div className="col s12 center-align">
+                                    <div>
+                                        { memberObjects.map((data, indx) => {
+                                            if (data.email !== this.props.auth.user.email) {
+                                                return (
+                                                    <h5 key={indx}> {data.name} </h5> 
+                                                )
+                                            }
+                                            return ''
+                                    
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </>) 
+                }
             </div>
-        )
+       )
     }
 }
 
