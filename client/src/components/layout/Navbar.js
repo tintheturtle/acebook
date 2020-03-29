703ae91678581084928c74cca675b239b1ccab18
tintheturtle <tinhoang@bu.edu>
2020-03-29 12:58:06 -0400

    added navbar for logged in users

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+
+import '../../styles/Navbar.css'
 
 class Navbar extends Component {
     render() {
+
+        const { email } = this.props.auth.user
+
+        if (email) {
+            return (
+                <div className="navbar-fixed">
+                <nav className="z-depth-o" style={{ backgroundColor: "white"}}>
+                    <div className="nav-wrapper-white">
+                        <Link
+                            to="/"
+                            style={{ 
+                                fontFamily: "monospace"
+                            }}
+                            className="col s5 brand-logo center black-text"
+                        >
+                            <i className="material-icons"> code </i>
+                            ACEBOOK
+                        </Link>
+                    </div>
+                </nav>
+            </div>
+            )
+        }
+
         return (
             <div className="navbar-fixed">
                 <nav className="z-depth-o" style={{ backgroundColor: "white"}}>
@@ -24,4 +52,10 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar
+Navbar.propTypes = {
+    auth: PropTypes.object.isRequired
+  }
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+export default connect(mapStateToProps)(Navbar)
