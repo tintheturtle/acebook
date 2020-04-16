3a9d538e1e57cab8bd4cb04f8ae607387629ee97
tintheturtle <tinhoang@bu.edu>
2020-04-16 16:16:55 -0400

    added minor deployment changes (changed secretKey for passport, added local mongoDB)

--- routes/api/users.js
+++ routes/api/users.js
@@ -152,7 +152,7 @@ router.post("/login", async (req, res) => {
 
                         jwt.sign(
                             payload,
-                            process.env.secretOrKey,
+                            'process.env.secretOrKey',
                             {
                                 expiresIn: 31556926
                             },
